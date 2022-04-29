e0e8697c6b9dca7e32af26543fd47a0c8164679a
Timothy <104185635+TimothyRab@users.noreply.github.com>
2022-04-29 11:56:05 +0000

    Crosshair
    Crosshair that rotates, beginnings of enemy shups

--- PerlinNoise.py
+++ PerlinNoise.py
@@ -2,10 +2,18 @@ import pygame, math, random
 from perlin_noise import PerlinNoise
 
 
-noise = PerlinNoise()
-array = [0]
-# Matricies Multiplication Function
+# TO DO ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+# Clean up terrain noise code to remove array function.
+# Improve culling method, try learning new python code for once 
 
+
+
+
+
+noise = PerlinNoise()
+
+
+# Matricies Multiplication Function
 
 def MatriciesMultiply(ma,mb):
     rowa = len(ma[0])
@@ -48,14 +56,21 @@ def Projection(z):
 
 
 
+def XYRotation(array,angle):
+    sin = math.sin(angle)
+    cos = math.cos(angle)
 
+    x = array[0] * cos - array[1] * sin
+    y = array[1] * cos + array[0] * sin
+
+    return (x,y)
 
 
 #Draw window.
 Length = 800
-Window = pygame.display.set_mode((Length , Length + 300))
+Window = pygame.display.set_mode((Length, Length))
 Half = (Length / 2)
-Zoom = 160
+FOV = 140
 
 # Set up the grid. Depth is not yet added.
 
@@ -69,21 +84,19 @@ def ManipulatePoints(x,y,z,rotx,roty,rotz):
     XYZ = MatriciesMultiply(RotationY(roty), XYZ)
     XYZ = MatriciesMultiply(RotationZ(rotz), XYZ)
     XYZ = MatriciesMultiply(Projection(XYZ[2]), XYZ)
-    XYZ[0] = XYZ[0] * Zoom + Half
-    XYZ[1] = XYZ[1] * Zoom + Half
+    XYZ[0] = XYZ[0] * FOV + Half
+    XYZ[1] = XYZ[1] * FOV + Half
     return XYZ
 
 # Draw the Line from the points that have been manipulated.
 
 def DrawLine(Point1,Point2, colour):
 
-    if Point1[0]< Length + 100:
-        if Point2[0] < Length + 100:
-            if Point1[0] > - 80:
-                if Point2[0] > - 80:
-                    if Point1[1] < Length + 350:
-                        if Point2[1] < Length + 350:
-                            pygame.draw.line(Window, colour, (Point1[0],Point1[1]),(Point2[0],Point2[1]))
+
+    if Point1[1] < Length or Point2[1] < Length:
+        if Point1[0] < Length or Point2[0] < Length:
+            if Point1[0] > 0 or Point2[0] > 0:
+                pygame.draw.line(Window, colour, (Point1[0],Point1[1]),(Point2[0],Point2[1]))
 
     
 
@@ -91,15 +104,14 @@ def DrawLine(Point1,Point2, colour):
 # Draws the grid by getting and manipulating points in 3D space, projecting them into 2D space and then joining them with a line.
 
 
-scalez = 24
-scalex = 22
-tilesize = 2
 
-def Ship(x,y,z,rotx,roty,rotz):
+
+def Ship(x,y,z,rotx,roty,rotz, colour):
     
-    Coord1 = ((-0.2,0,0,2.6,0,0),(0.2,0,0,2.6,0,0),(-0.2,0,-2,2.6,0,0),(0.2,0,-2,2.6,0,0),
-              (-0.2,0.1,0,2.6,0,0),(0.2,0.1,0,2.6,0,0),(-0.2,0.1,-2,2.6,0,0),(0.2,0.1,-2,2.6,0,0),
-              (-0.5,0.05,-2,2.6,0,0),(0.5,0.05,-2,2.6,0,0),(-0.2,0.05,0,2.6,0,0),(0.2,0.05,0,2.6,0,0))
+    Coord1 = ((-0.2,0,0,2.6,0,0),(0.2,0,0,2.6,0,0),(-0.2,0,-0.5,2.6,0,0),(0.2,0,-0.5,2.6,0,0),
+              (-0.2,0.1,0,2.6,0,0),(0.2,0.1,0,2.6,0,0),(-0.2,0.1,-0.5,2.6,0,0),(0.2,0.1,-0.5,2.6,0,0),
+              (-0.5,0.05,-0.5,2.6,0,0),(0.5,0.05,-0.5,2.6,0,0),(-0.2,0.05,-0.2,2.6,0,0),(0.2,0.05,-0.2,2.6,0,0),
+               (-0.1,0.1,0.1,2.6,0,0), (0.1,0.1,0.1,2.6,0,0))
     Coord2 = []
     i = 0
     while i < len(Coord1):
@@ -108,7 +120,6 @@ def Ship(x,y,z,rotx,roty,rotz):
         #print(Coord2[i])
         i += 1
     
-    colour = (0,170,255)
 
     # Base
     DrawLine(Coord2[1], Coord2[3], colour)
@@ -138,13 +149,25 @@ def Ship(x,y,z,rotx,roty,rotz):
     DrawLine(Coord2[8],Coord2[10],colour)
     DrawLine(Coord2[9],Coord2[11],colour)
 
+    # Cockpit
+    DrawLine(Coord2[0],Coord2[12],colour)
+    DrawLine(Coord2[1],Coord2[13],colour)
+    DrawLine(Coord2[4],Coord2[12],colour)
+    DrawLine(Coord2[5],Coord2[13],colour)
+    DrawLine(Coord2[12],Coord2[13],colour)
 
 
-def TerrainNoise(x,y,z):
-    tn = y + (7 * noise([x/10,(z + array[0]) / 10]))
+
+
+def TerrainNoise(x,y,z, perlinz):
+    tn = y + (7 * noise([x/10,(z + perlinz) / 10]))
     return tn
 
-def DrawGrid(x,y,z,rotx,roty,rotz):
+scalez = 20
+scalex = 20
+tilesize = 2
+
+def DrawGrid(x,y,z,rotx,roty,rotz, perlinz):
     pygame.Surface.fill(Window,(50,0,50))
     
     colour = 255
@@ -152,27 +175,27 @@ def DrawGrid(x,y,z,rotx,roty,rotz):
 
         x = -1 * scalex
         
-        colour -= 15
+        colour -= 18
         while x < scalex:
             
             
             # Draw horizontal line
-            Point1 = ManipulatePoints(x, TerrainNoise(x,y,z), z,rotx,roty,rotz)
-            Point2 = ManipulatePoints(x + tilesize, TerrainNoise(x + tilesize,y,z), z,rotx,roty,rotz)
+            Point1 = ManipulatePoints(x, TerrainNoise(x,y,z,perlinz), z,rotx,roty,rotz)
+            Point2 = ManipulatePoints(x + tilesize, TerrainNoise(x + tilesize,y,z, perlinz), z,rotx,roty,rotz)
             
             DrawLine(Point1, Point2, (colour,0,colour))
 
             # Draw vertical line
 
-            Point1 = ManipulatePoints(x, TerrainNoise(x,y,z), z,rotx,roty,rotz)
-            Point2 = ManipulatePoints(x, TerrainNoise(x,y,z + tilesize), z + tilesize,rotx,roty,rotz)
+            Point1 = ManipulatePoints(x, TerrainNoise(x,y,z,perlinz), z,rotx,roty,rotz)
+            Point2 = ManipulatePoints(x, TerrainNoise(x,y,z + tilesize,perlinz), z + tilesize,rotx,roty,rotz)
             
             DrawLine(Point1, Point2, (colour,0,colour))
             
             # Create diagonal line
 
-            Point1 = ManipulatePoints(x + tilesize, TerrainNoise(x + tilesize,y,z + tilesize), z + tilesize,rotx,roty,rotz)
-            Point2 = ManipulatePoints(x, TerrainNoise(x,y,z), z,rotx,roty,rotz)
+            Point1 = ManipulatePoints(x + tilesize, TerrainNoise(x + tilesize,y,z + tilesize,perlinz), z + tilesize,rotx,roty,rotz)
+            Point2 = ManipulatePoints(x, TerrainNoise(x,y,z,perlinz), z,rotx,roty,rotz)
             
             DrawLine(Point1, Point2, (colour,0,colour))
 
@@ -181,24 +204,58 @@ def DrawGrid(x,y,z,rotx,roty,rotz):
         z += tilesize
 
 
+# (-1 * (Mouse[1] - (Length/2)) / 4000)
+
+def Reticle(Mouse,angle):
+
+    angle = angle / 10
+
+    x = Mouse[0]
+    y = Mouse[1]
+    i = 0
+    while i < 4:
+        Line1 = XYRotation((0, -5),angle + ((math.pi / 2) * i))
+        Line2 = XYRotation((0,-20), angle + ((math.pi / 2) * i))
+        DrawLine((Line1[0] + x,Line1[1] + y),(Line2[0] + x,Line2[1] + y), (0,170,255))
+
+        Line1 = XYRotation((0, -5),angle + ((math.pi / 2) * i))
+        Line2 = XYRotation((+5,-20),angle + ((math.pi / 2) * i))
+        DrawLine((Line1[0] + x,Line1[1] + y),(Line2[0] + x,Line2[1] + y), (0,170,255))
+
+        Line1 = XYRotation((0, -20),angle + ((math.pi / 2) * i))
+        Line2 = XYRotation((+5,-20), angle + ((math.pi / 2) * i))
+        DrawLine((Line1[0] + x,Line1[1] + y),(Line2[0] + x,Line2[1] + y), (0,170,255))
+        
+        i += 1
+
+    
+
+
 
 # Draw and update window loop
 def main():
 
     run = True
     change = 0
-    while run:
-        
-        pygame.display.flip()   
-        
-        change += 0.1
-        
-        DrawGrid(0,-5,-1,2.6,0,0)
-        Ship(1,-0.5,change,0,0,0)
-        pygame.draw.line(Window,(155,155,155),(0,800),(800,800))
-        pygame.draw.line(Window,(155,155,155),(800,0),(800,800))
 
-        array[0] += 0.1
+
+    while run:
+
+        Mouse = pygame.mouse.get_pos()
+        pygame.mouse.set_visible(False)
+
+        change += 1
+        # Terrain
+        DrawGrid(0,-5,-1,2.6,0,0, change)
+        # Enemy Ship
+        Ship(0.5,0,1.5,2.6,math.pi / 2,0,(255,0,0))
+        Ship(-0.5,0,1.5,2.6,math.pi / 2,0,(255,0,0))
+        # Player Character
+        Ship(0 + (-1 * (Mouse[0] - (Length/2)) / 400),0 + (-1 * (Mouse[1] - (Length/2)) / 400),-2,2.6,0,0,(0,170,255))
+
+        Reticle(Mouse,change)
+
+        pygame.display.flip()  
 
         for event in pygame.event.get():
 
