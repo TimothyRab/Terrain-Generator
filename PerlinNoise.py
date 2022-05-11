eabe5af471a09586dc0171c9777b90c5fe246be6
Timothy <104185635+TimothyRab@users.noreply.github.com>
2022-05-11 10:51:52 +0000

    Render Optimizations and Additions
    Optimized projection and manipulating points functions, reducing unnecessary lines.
    Re-wrote grid mesh code, making scaling functional again, and reversed the order which the grid is rendered, eliminating layer artifacts.
    Added an FPS counter, which prints FPS and frame times to the console.

--- PerlinNoise.py
+++ PerlinNoise.py
@@ -3,7 +3,6 @@ from perlin_noise import PerlinNoise
 
 # +++++++++++ TO DO ++++++++++++
 # Smart optimizations for rendering, compute less math, especially math that would otherwise be wasted
-# == Check tile sizes and scale. Possible optimization opportunity?
 # Configure AI to use fixed systems, possibly create more modular and independent functions
 
 
@@ -30,31 +29,6 @@ def MatriciesMultiply(ma,mb):
     return mc
 
 
-# Rotation and Projection Matricies
-def RotationZ(rotation):
-    rotationZ = ((math.cos(rotation), -1 * math.sin( rotation),0), (math.sin(rotation),math.cos(rotation), 0), (0,0,1))
-    return rotationZ
-
-def RotationX(rotation):
-    rotationX = ((1,0,0), (0,math.cos(rotation),-1 * math.sin(rotation)), (0,math.sin(rotation),math.cos(rotation)))
-    return rotationX
-
-def RotationY(rotation):
-    rotationY = ((math.cos(rotation),0,math.sin(rotation)), (0,1,0), (math.sin(rotation),0,math.cos(rotation)))
-    return rotationY
-
-def Projection(point):
-
-    Projection = ((1 / (distance - point[2]),0,0),(0,1 / (distance - point[2]),0),(0,0,0))
-    return Projection
-
-
-def PreCalcRot(rotation):
-    rotlist = []
-    rotlist.append(RotationX(rotation[0]))
-    rotlist.append(RotationY(rotation[1]))
-    rotlist.append(RotationZ(rotation[2]))
-    return rotlist
 
 #Draw window.
 Length = 800
@@ -63,6 +37,20 @@ Half = (Length / 2)
 FOV = 100
 
 # Manipulate the points in 3D space, before projecting them to the Window.
+
+def CalcRot(rotation):
+    cosrotX = math.cos(rotation[0])
+    sinrotX = math.sin(rotation[0])
+
+    cosrotY = math.cos(rotation[1])
+    sinrotY = math.sin(rotation[1])
+
+    cosrotZ = math.cos(rotation[2])
+    sinrotZ = math.sin(rotation[2])
+
+    rotlist = [((1,0,0), (0,cosrotX,-1 * sinrotX), (0,sinrotX,cosrotX)),((cosrotY,0,sinrotY), (0,1,0), (sinrotY,0,cosrotY)),((cosrotZ, -1 * sinrotZ,0), (sinrotZ,cosrotZ, 0), (0,0,1))]
+    return rotlist
+
 
 def ManipulatePoints(point,rotation,translation):
 
@@ -75,12 +63,17 @@ def ManipulatePoints(point,rotation,translation):
     point[1] += translation[1]
     point[2] += translation[2]
 
-    point = MatriciesMultiply(Projection(point),point)
+    point = MatriciesMultiply(((1 / (distance - point[2]),0,0),(0,1 / (distance - point[2]),0),(0,0,0)),point)
 
     point[0] = point[0] * FOV + Half
     point[1] = point[1] * FOV + Half
     
     return point
+
+
+
+
+
 
 # Draw the Line from the points that have been manipulated.
 
@@ -94,16 +87,16 @@ def DrawLine(Point1,Point2, colour):
 
 def Ship(translation,rotation,colour):
 
-    Coord1 = [(-0.25,0,0.5),(0.25,0,0.5),(-0.25,0,-0.5),(0.25,0,-0.5),
-              (-0.25,0.1,0.5),(0.25,0.1,0.5),(-0.25,0.1,-0.5),(0.25,0.1,-0.5),
-              (-0.5,0.05,-0.5),(0.5,0.05,-0.5),(-0.25,0.05,0),(0.25,0.05,0),
-               (-0.1,0,1), (0.1,0,1)]
+    Coord1 = [(-0.25,0,0.25),(0.25,0,0.25),(-0.25,0,-0.25),(0.25,0,-0.25),
+              (-0.25,0.1,0.25),(0.25,0.1,0.25),(-0.25,0.1,-0.25),(0.25,0.1,-0.25),
+              (-0.5,0.05,-0.25),(0.5,0.05,-0.25),(-0.25,0.05,0),(0.25,0.05,0),
+               (-0.1,0,0.5), (0.1,0,0.5)]
 
     
     i = 0
     while i < len(Coord1):
         
-        Coord1[i] = (ManipulatePoints(Coord1[i],PreCalcRot(rotation),translation))
+        Coord1[i] = (ManipulatePoints(Coord1[i],CalcRot(rotation),translation))
         i += 1
 
     DrawLine(Coord1[1], Coord1[3], colour)
@@ -144,50 +137,58 @@ def Ship(translation,rotation,colour):
 
 # Parameters for the scale of the grid and amount of squares on both axis
 
-scalez = 15
+scalez = 10
 scalex = 20
-tilesize = 2
+tilesize = 50
 
 # Draws the grid by getting and manipulating points in 3D space, projecting them into 2D space and then joining them with a line.
 
-def DrawGrid(translation,rotation):
-    x = 0
+def DrawGrid(translation,rotation,timer):
+    x = scalex / 2 * -1
     y = translation[1]
     z = 0
-    Coordinates = []
-    rotation = PreCalcRot(rotation)
+    Column = []
+    rotation = CalcRot(rotation)
+
     while z < scalez:
+        x = scalex / 2 * -1
+        Row = []
+        while x != scalex / 2 + 1:
 
-        x = -1 * scalex
-
-        while x < scalex:
-
-            zcornerplus = z + tilesize
-            xcornerplus = x + tilesize
-
-            translation[1] = y + 7 * noise([x/10,z / 10])
-            Coordinates.append(ManipulatePoints((x, 0, z),rotation,translation))
-
-            x += tilesize
-    
-        z += tilesize
-    
-    i = 0
-    while i != len(Coordinates):
-
-        #print(i)
-        if i  + 1< len(Coordinates) and i + 1 != scalex * round(i / scalex):
+            translation[1] = y + tilesize * noise([x/3,(z + timer / 10) / 3])
+            point = ManipulatePoints((x * tilesize, 0, z * tilesize),rotation,translation)
             
-            DrawLine(Coordinates[i],Coordinates[i + 1],(255,0,0))
+            colour =  240 - 200 * (z / scalez)
+            
+            point[2] = (colour,0,colour)
+            
+            Row.append(point)
 
-        if i + scalex < len(Coordinates):
-            DrawLine(Coordinates[i],Coordinates[i + scalex],(0,255,0))
+            x += 1
 
-        if i + scalex + 1 < len(Coordinates) and i + 1 != scalex * round(i / scalex):
-         DrawLine(Coordinates[i],Coordinates[i + scalex + 1],(0,0,255))
-        
-        i += 1
-        
+        Column.append(Row)
+        z += 1
+
+    
+    
+    z = scalez - 1
+    x = scalex
+
+
+    while z > -1:
+        x = scalex
+        while x > -1:
+            
+            if x > 0:
+                DrawLine(Column[z][x],Column[z][x - 1],Column[z][x][2])
+            if z > 0:
+                DrawLine(Column[z][x],Column[z - 1][x],Column[z][x][2])
+            if x > 0 and z > 0:
+                DrawLine(Column[z][x],Column[z - 1][x - 1],Column[z][x][2])
+            x -= 1
+            
+        z -= 1
+
 
 
 # Draw a rotating reticle on the screen
@@ -197,15 +198,31 @@ def Reticle(Mouse,angle):
     angle = angle / 10
 
     Coord1 = ((0,0.05,0),(0,0.2,0),(0.05, 0.2,0))
-    Translation = ((Mouse[0] / Length - 0.5) * 7,(Mouse[1] / Length - 0.5) * 7,0)
+    Translation = ((Mouse[0] / Length - 0.5) * 8,(Mouse[1] / Length - 0.5) * 8,0)
     i = 0
     while i < 4:
-        Rotation = PreCalcRot((0,0,i * math.pi / 2 + angle))
+        Rotation = CalcRot((0,0,i * math.pi / 2 + angle))
         DrawLine(ManipulatePoints(Coord1[0], Rotation, Translation), ManipulatePoints(Coord1[1], Rotation, Translation), (0,170,255))
         DrawLine(ManipulatePoints(Coord1[0], Rotation, Translation), ManipulatePoints(Coord1[2], Rotation, Translation), (0,170,255))
         DrawLine(ManipulatePoints(Coord1[1], Rotation, Translation), ManipulatePoints(Coord1[2], Rotation, Translation), (0,170,255))
         i += 1
+
+class EnemyShip():
+
+
+    def __init__(self,initx,inity,initz):
+        self.x = initx
+        self.y = inity
+        self.z = initz
+
+
+    
+
+    def update(self, timer):
         
+        Ship((self.x,self.y,self.z),(0,math.pi,0),(255,0,0))
+
+
 # Class for Playership
 class PlayerShip():
 
@@ -224,33 +241,39 @@ class PlayerShip():
 
 
 player = PlayerShip(0, 0, 0)
-
+enemy = EnemyShip(0,0,2)
 
 
 # Draw and update window loop
 def main():
     last_time = time.time()
+    frame = 0
+    second = 0
     run = True
     timer = -10
     pygame.mouse.set_visible(False)
-
     while run:
         
         dt = time.time() - last_time
         last_time = time.time()
-        print(dt * 60)
+
+
         pygame.Surface.fill(Window,(50,0,50))
 
         Mouse = pygame.mouse.get_pos()
         
+        print('FPS: ' + str(round(1 / dt)) + " Frametime: " + str(dt))
 
-        timer += 1
+        
+
+        timer += 1 * dt * 60
 
         #Terrain
-        DrawGrid([0,3,0],(2.6,0,0))
+        DrawGrid([0,30,0.-2],(math.pi/4 * 5,0,0),timer)
 
         Reticle(Mouse,timer)
         player.update(Mouse)
+        enemy.update(timer)
 
         pygame.display.flip()  
 
